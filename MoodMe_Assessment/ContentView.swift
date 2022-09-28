9557612b6178e9ccb65314cff928cbc55c7a8022
RayPas <70732899+RayPas@users.noreply.github.com>
2022-09-28 16:30:55 -0700

    First Commit

--- MoodMe_Assessment/ContentView.swift
+++ MoodMe_Assessment/ContentView.swift
@@ -4,23 +4,11 @@
 //
 //  Created by Raymond Pasion on 9/28/22.
 //
-
+import AVFoundation
 import SwiftUI
+import PhotosUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+var session: AVCaptureSession?
+let output = AVCapturePhotoOutput()
+let previewLayer = AVCaptureVideoPreviewLayer()
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
